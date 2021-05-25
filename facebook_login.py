fc44d8de12f7748666af80bf92405e457a21e871
physicistgaurav <58541817+physicistgaurav@users.noreply.github.com>
2021-05-25 11:01:04 +0545

    Update facebook_login.py

--- facebook_login.py
+++ facebook_login.py
@@ -6,10 +6,10 @@ driver = webdriver.Chrome()
 driver.get('https://www.facebook.com/')
 
 search_user = driver.find_element_by_xpath('//*[@id="email"]')
-search_user.send_keys('physicistgaurav')
+search_user.send_keys('your email or username')
 
 search_pswd = driver.find_element_by_xpath('//*[@id="pass"]')
-search_pswd.send_keys('hello')
+search_pswd.send_keys('your password')
 
 
 search_button = driver.find_element_by_name("login")
