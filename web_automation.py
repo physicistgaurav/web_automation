8418fd4d540445aafdd9172c94b0a749cf7c689c
physicistgaurav <58541817+physicistgaurav@users.noreply.github.com>
2021-05-25 11:03:51 +0545

    Update web_automation.py

--- web_automation.py
+++ web_automation.py
@@ -37,64 +37,15 @@
 # driver.get('https://www.facebook.com/')
 #
 # search_user = driver.find_element_by_xpath('//*[@id="email"]')
-# search_user.send_keys('physicistgaurav')
+# search_user.send_keys('your emal')
 #
 # search_pswd = driver.find_element_by_xpath('//*[@id="pass"]')
-# search_pswd.send_keys('hello')
+# search_pswd.send_keys('your password')
 #
 #
 # search_button = driver.find_element_by_name("login")
 # search_button.click()
 
-# #Meroshare
-
-# from selenium import webdriver
-# from selenium.webdriver.support.select import Select
-#
-# driver = webdriver.Chrome()
-# driver.implicitly_wait(10)
-# driver.get('https://meroshare.cdsc.com.np/#/login')
-#
-#
-# # search_dp_button= driver.find_element_by_xpath('//*[@id="select2-uumj-container"]')
-# # search_dp_button.click()
-# # search_dp_button.send_keys('11100')
-#
-#
-#
-# search_dp_button= driver.find_element_by_name('username')
-# search_dp_button.send_keys('00482651')
-#
-# search_dp_button= driver.find_element_by_name('password')
-# search_dp_button.send_keys('lizardking@27')
-#
-#
-#
-# # search_dp_button= driver.find_element_by_class_name('select2-selection__rendered')
-# # search_dp_button.click()
-# # search_dp_button=driver.find_element_by_class_name('select2-search__field')
-# # search_dp_button.send_keys('11100')
-#
-# search_dp_button= driver.find_element_by_class_name('select2-selection__rendered')
-# search_dp_button.click()
-# # search_dp_button=driver.find_element_by_class_name('select2-search__field')
-#
-#
-# sel=Select(driver.find_element_by_class_name('select2-hidden-accessible'))
-# # search_dp_button.send_keys('11100')
-#
-#
-# sel.select_by_visible_text("BANK OF KATHMANDU LIMITED (11100)")
-#
-# search_dp_button= driver.find_element_by_class_name('select2-selection__rendered')
-# search_dp_button.click()
-#
-#
-# search_dp_button= driver.find_element_by_css_selector('.btn')
-# search_dp_button.click()
 
 
-
-
-# #Send automatic message
 
